8e28f01dbec357cdf7f7ddb374be4dbd678837ec
Joaquin V <joaquin.valdivieso.96@gmail.com>
2020-09-16 00:03:08 -0400

    uso de useEffect

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const Nosotros = () => {
+
+    const [equipo, setEquipo] = React.useState([])
+
+    const obtenerDatos = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await data.json()
+        //console.log(users)
+        setEquipo(users)
+        console.log(equipo)
+    }
+
+    React.useEffect(()=>{
+        obtenerDatos()
+    }, [])
+
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map(item => () => (
+                    <li key={item.id}> {item.name} - {item.email}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
