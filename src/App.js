e6a840f392281ca832f3bd9f9606f183b6b816ec
Joaquin V <joaquin.valdivieso.96@gmail.com>
2020-09-15 22:10:15 -0400

    Proyecto de React

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Inicio from './components/Inicio';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
+
+function App() {
+  return (
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">
+            Inicio
+          </Link>
+          <Link to="/contacto" className="btn btn-dark">
+            Contacto
+          </Link>
+          <NavLink to="/nosotros" className="btn btn-dark" activeClassName="active">
+            Nosotros
+          </NavLink>
+        </div>
+        <hr/>
+        <Switch>
+          <Route path="/contacto" exact>
+            <Contacto/>
+          </Route>
+          <Route path="/nosotros" exact>
+            <Nosotros/>
+          </Route>
+          <Route path="/" exact>
+            <Inicio/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
